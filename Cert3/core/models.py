22206718f027fbacdbe0f0aece0d9f2e72da9f2c
CamiloBCrep <camilo.bilbao@sansano.usm.cl>
2024-07-05 05:08:38 -0400

    Cambios v2

--- Cert3/core/models.py
+++ Cert3/core/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 from django.utils import timezone
 
-combustible_choices = (('PRG'),('PRD'),('PRA'))
+
 
 class nuevo_proyecto(models.Model):
     estudiante=models.TextField(max_length=50)
@@ -9,9 +9,20 @@ class nuevo_proyecto(models.Model):
     proy=models.TextField(max_length=255)
     tema=models.TextField(max_length=50)
 
+CODIGO_CHOICES = (('PRG'),('PRD'),('PRA'))
+TURNO_CHOICES = (('AM'),('PM'),('MM'))
+
 class combustible(model.Model):
-    codigo = models.TextField(max_length=3)
+    codigo = models.ChoiceField(
+        max_length=3,
+        choices=CODIGO_CHOICES,
+        default='-'
+    )
     litros = models.IntegerField()
-    fecha_hora = DateTimeField(auto_now_add=True)
-    operario = models.TextField(max_length=40)
-#    turno = (agregar choose box)
+    fecha_hora = models.DateTimeField(auto_now_add=True)
+    operario = models.ForeignKey(User, on_delete=models.CASCADE)
+    turno = models.ChoiceField(
+        max_length=2,
+        choices=TURNO_CHOICES,
+        default='-'
+    )
--- Cert3/core/views.py
+++ Cert3/core/views.py
@@ -3,6 +3,7 @@ from .models import nuevo_proyecto, combustible
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login
 from django.contrib import messages
+import datetime
 # Create your views here.
 def home(request):
     title = "inicio"
@@ -10,6 +11,7 @@ def home(request):
         "title":title
     }
     return render(request, 'core/home.html', data)
+    
 def login_view(request):
     if request.method == 'POST':
         username = request.POST.get('username')
@@ -28,35 +30,38 @@ def exit(request):
     logout(request)
     return redirect('home')
 
-def new_proyecto(request):
-    if(request.POST):
-        #capturar datos desde el formulario
-        estudiante = request.POST['txtEstudiante']
-        proy = request.POST['txtProyecto']
-        tema = request.POST['txtTema']
+def crear_registro(request):
+    if request.method == 'POST':
 
-        #validaciones de reglas de negocio
+        codigo = request.POST.get('cmbCodigo')
+        litros = request.POST.get('txtLitros')
+        
+        now = datetime.datetime.now()
 
-        #construir y cargar el objeto con los datos del form
-        proyecto = nuevo_proyecto(estudiante=estudiante, proy=proy, tema=tema)
+        if now.hour < 6:
+            turno = 'MM'
+        elif now.hour < 12:
+            turno = 'MM'
+        else:
+            turno = 'PM'
 
+        if not name or not category_type:
+            messages.error(request, 'Por favor completa todos los campos del formulario.')
+        else:
+            combustible.objects.Create(
+                codigo=codigo,litros=litros,
+                fecha_hora=fecha_hora,
+                operario=operario,
+                tuno=turno
+            )
+            
+            messages.success(request, '¡La el registro se ha creado exitosamente!')
+            return redirect('core/home.html')
+    return render(render, 'core/formulario.html')
 
-        proyecto.save()
-    return render(request, 'core/proyecto.html')
-
-def listar_proyectos(request):
+def listar_registros(request):
     # Obtener todos los proyectos de la base de datos
-    data = nuevo_proyecto.objects.all()
-    context = {'proyectos': data}
+    data = combustible.objects.all()
+    context = {'registros': data}
     # Pasar los proyectos al contexto para que estén disponibles en la plantilla
     return render(request, 'core/home.html', context)
-
-def crear_registro(request):
-    codigo = request.POST[]
-    litros = request.POST[]
-    fecha_hora = request.POST[]
-    operario = request.POST[]
-    turno = request.POST[]
-    nuevo_registro = combustible(codigo=codigo,litros=litros,fecha_hora=fecha_hora,operario=operario,tuno=turno)
-    nuevo_registro.save()
-    return render(render, 'core/proyecto.html')
